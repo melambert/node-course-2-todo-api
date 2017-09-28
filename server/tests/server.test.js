fc35ab79e7bf081cc9686dbd25c0288314942a57
Mark <mark.e.lambert@googlemail.com>
2017-09-28 12:18:29 +0100

    Added delete test cases

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -92,3 +92,38 @@ describe('GET /todos/:id', () => {
             .end(done);
     })
 });
+
+describe('DELETE /todos/:id', () => {
+    it('should delete todo by id', (done) => {
+        var hexId = todos[1]._id.toHexString()
+        request(app)
+            .delete(`/todos/${hexId}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo._id).toBe(hexId);
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+                Todo.findById(hexId).then((todo) => {
+                    expect(todo).toNotExist();
+                    done();
+                }).catch((e) => done(e));
+            });
+    });
+
+    it('should return 404 if todo not found', (done) => {
+        request(app)
+            .delete(`/todos/${new ObjectID().toHexString()}`)
+            .expect(404)
+            .end(done);
+    });
+
+    it('should return 404 for non object ids', (done) => {
+        request(app)
+            .delete(`/todos/12u3u4o234340`)
+            .expect(404)
+            .end(done);
+    })
+});
