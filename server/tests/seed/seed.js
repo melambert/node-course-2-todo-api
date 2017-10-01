4172a9397c41ca28c70fe85f139672f6139f7ae3
Mark <mark.e.lambert@googlemail.com>
2017-10-01 19:41:14 +0100

    Test cases for user routes and authenication

---
+++ server/tests/seed/seed.js
@@ -0,0 +1,53 @@
+const {ObjectID} = require('mongodb');
+const jwt = require('jsonwebtoken');
+
+const {Todo} = require('./../../models/todo');
+const {User} = require('./../../models/user');
+
+const userOneId = new ObjectID();
+const userTwoId = new ObjectID();
+
+const users = [
+    {
+        _id: userOneId,
+        email: 'fu@fu.com',
+        password: 'userOnePass',
+        tokens: [{
+            access: 'auth',
+            token: jwt.sign({_id: userOneId, access: 'auth'},  'abc123secret').toString()
+        }]
+    }, {
+        _id: userTwoId,
+        email: 'fu2@fu.com',
+        password: 'userTwoPass',
+    }
+];
+
+const todos = [{
+    _id: new ObjectID(),
+    text: 'First task to do'
+}, {
+    _id: new ObjectID(),
+    text: 'Second task to do',
+    completed: true,
+    completedAt: 333
+}];
+
+const populateTodos = (done) => {
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(todos);
+    }).then(() => done());
+};
+
+const populateUsers = (done) => {
+    // Insert many won't call hashing middleware
+    User.remove({}).then(() => {
+       var userOne = new User(users[0]).save();
+       var userTwo = new User(users[1]).save();
+
+       return Promise.all([userOne, userTwo])
+    }).then(() => done());
+};
+
+
+module.exports = {todos, populateTodos, users, populateUsers};
